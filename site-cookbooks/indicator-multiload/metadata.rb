5adb7fcb9b2951f44b93d0037cb9e7e751fd0cac
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-04-26 01:42:29 +0900

    Create and use my indicator-multiload cookbook

---
+++ site-cookbooks/indicator-multiload/metadata.rb
@@ -0,0 +1,4 @@
+name             'indicator-multiload'
+description      'Installs/Configures indicator-multiload'
+version          '0.1.0'
+supports         'ubuntu'
---
+++ site-cookbooks/indicator-multiload/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: indicator-multiload
+# Recipe:: default
+#
+
+package "indicator-multiload" do
+  action :install
+end
