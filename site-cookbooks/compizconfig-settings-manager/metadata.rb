77423f2ec2cde2d00a17abb9e28f4bef7535973e
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-04-26 01:31:55 +0900

    Create and use my ccsm recipe

---
+++ site-cookbooks/compizconfig-settings-manager/metadata.rb
@@ -0,0 +1,4 @@
+name             'compizconfig-settings-manager'
+description      'Installs/Configures compizconfig-settings-manager'
+version          '0.1.0'
+supports         'ubuntu'
---
+++ site-cookbooks/compizconfig-settings-manager/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: compizconfig-settings-manager
+# Recipe:: default
+#
+
+package "compizconfig-settings-manager" do
+  action :install
+end
