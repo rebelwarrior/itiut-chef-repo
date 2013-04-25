80e780f981e4266c031387d7e986e8d4306dcb19
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-04-26 01:25:12 +0900

    Create and use my tig recipe

---
+++ site-cookbooks/tig/metadata.rb
@@ -0,0 +1,4 @@
+name             'tig'
+description      'Installs/Configures tig'
+version          '0.1.0'
+supports         'ubuntu'
---
+++ site-cookbooks/tig/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: tig
+# Recipe:: default
+#
+
+package "tig" do
+  action :install
+end
