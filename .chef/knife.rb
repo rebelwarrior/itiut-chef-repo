ec08a150e95feb5add1640856fb98c153ff04fbd
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-04-25 23:10:03 +0900

    delete unnecessary options

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,11 +3,7 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                'itiut'
-client_key               '/home/itiut/itiut-chef-repo/.chef/itiut.pem'
-validation_client_name   'chef-validator'
-validation_key           '/etc/chef/validation.pem'
 chef_server_url          'http://localhost:4000'
-syntax_check_cache_path  '/home/itiut/itiut-chef-repo/.chef/syntax_check_cache'
 
 cookbook_path [ 
   "#{current_dir}/../cookbooks",
