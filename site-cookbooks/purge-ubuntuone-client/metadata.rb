f46fd0aa3fbdc0eb399c01a2da9a3d3d00764584
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-05-01 02:43:03 +0900

    Purge ubuntuone-client

---
+++ site-cookbooks/purge-ubuntuone-client/metadata.rb
@@ -0,0 +1,4 @@
+name             'purge-ubuntuone-client'
+description      'Purge ubuntuone-client'
+version          '0.1.0'
+supports         'ubuntu'
---
+++ site-cookbooks/purge-ubuntuone-client/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: purge-ubuntuone-client
+# Recipe:: default
+#
+
+package "ubuntuone-client" do
+  action :purge
+end
