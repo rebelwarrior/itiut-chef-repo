8ff2348e71b32d7533821d6861b1c4a2ab7fe312
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-05-01 02:38:55 +0900

    Purge unity-webapps-service

---
+++ site-cookbooks/purge-unity-webapps-service/metadata.rb
@@ -0,0 +1,4 @@
+name             'purge-unity-webapps-service'
+description      'Purge unity-webapps-service'
+version          '0.1.0'
+supports         'ubuntu'
---
+++ site-cookbooks/purge-unity-webapps-service/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: purge-unity-webapps-service
+# Recipe:: default
+#
+
+package "unity-webapps-service" do
+  action :purge
+end
