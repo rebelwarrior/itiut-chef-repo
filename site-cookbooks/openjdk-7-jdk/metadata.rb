23609719073ad6e73e1b31e889f6cd65f6b8aa8c
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-05-30 13:51:35 +0900

    Add openjdk-7-jdk

---
+++ site-cookbooks/openjdk-7-jdk/metadata.rb
@@ -0,0 +1,4 @@
+name             'openjdk-7-jdk'
+description      'Installs/Configures openjdk-7-jdk'
+version          '0.1.0'
+supports         'ubuntu'
---
+++ site-cookbooks/openjdk-7-jdk/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: openjdk-7-jdk
+# Recipe:: default
+#
+
+package "openjdk-7-jdk" do
+  action :install
+end
